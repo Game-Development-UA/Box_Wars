80e1dcf50dc73f273f0bfc4fc833374b9ab12c07
Vishal Shelat <vishalshelat@gmail.com>
2018-10-28 01:17:43 -0700

    added scripts for auto generation

--- Assets/Sprites/ToonBot/ToonBot(Free) Assets/Controller/MC_movement.cs
+++ Assets/Sprites/ToonBot/ToonBot(Free) Assets/Controller/MC_movement.cs
@@ -7,83 +7,77 @@ using UnityEngine;
 
 //[RequireComponent(typeof(AudioSource))]
 public class MC_movement : MonoBehaviour {
-	public float moveSpeed;
-	public float horizMoveSpeed;
-    public int lanenum = 2;
+    public KeyCode moveL;
+    public KeyCode moveR;
+   // public int lanenum = 2;
     public string controlLocked = "n";
-
-    public Rigidbody body;
 
     //death particle
     public Transform boomObj;
 
-    float horiz = 0;
+    public float hvelocity = 0;
     // Use this for initialization
-    // void Start () {
-    //     body.velocity = new Vector3(0, GM.vertVel, 4);
-    // }
+    void Start () {
+        GetComponent<Rigidbody>().velocity = new Vector3(0, GM.vertVel, 4);
+    }
     
     // Update is called once per frame
     void Update () {
-    	horiz = Input.GetAxis( "Horizontal" ) * horizMoveSpeed;
+        GetComponent<Rigidbody>().velocity = new Vector3 (hvelocity, GM.vertVel , 4);
 
-        // if (Input.GetKeyDown (moveL) && (lanenum > 1) && (controlLocked == "n")){
-        //     hvelocity = -2;//move left
-        //     StartCoroutine (stopSlide ());
-        //     lanenum -= 1;
-        //     controlLocked = "y";
-        // }
+       // if (Input.GetKeyDown (moveL) && (lanenum > 1) && (controlLocked == "n")){
+            hvelocity = -2;{//move left
+            StartCoroutine (stopSlide ());
+         //   lanenum -= 1;
+            controlLocked = "y";
+        }
 
-        // if (Input.GetKeyDown (moveR) && (lanenum < 3) && (controlLocked == "n")){
-        //     hvelocity = 2;//move left
-        //     StartCoroutine (stopSlide ());
-        //     lanenum += 1;
-        //     controlLocked = "y";
-        // }
-    }
-
-    void FixedUpdate() {
-    	body.velocity = new Vector3( horiz, body.velocity.y, moveSpeed );
+       // if (Input.GetKeyDown (moveR) && (lanenum < 3) && (controlLocked == "n")){
+            hvelocity = 2;{//move left
+            StartCoroutine (stopSlide ());
+         //   lanenum += 1;
+            controlLocked = "y";
+        }
     }
 
     void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Lethal") {
             Destroy (gameObject);
-            // GM.zVelAdj = 0;
+            GM.zVelAdj = 0;
             Instantiate(boomObj, transform.position, boomObj.rotation);
             GM.lvlCompStatus = "Fail!";
         }
-        // if (other.gameObject.tag == "Powerup") {
-        //     Destroy(other.gameObject);
-        //     //add powerup effects
-        // }
+        if (other.gameObject.name == "Powerup(Clone)" || other.gameObject.name == "Powerup") {
+            Destroy(other.gameObject);
+            //add powerup effects
+        }
     }
 
     void OnTriggerEnter(Collider other){
         //vertical movement on ramp
-        // if (other.gameObject.name == "RampBotTrigger") {
-        //     GM.vertVel = 1.7f;
-        // }
-        // // stop vert movement after ramp
-        // if (other.gameObject.name == "RampTopTrigger") {
-        //     GM.vertVel = 0;
-        // }
+        if (other.gameObject.name == "RampBotTrigger") {
+            GM.vertVel = 1.7f;
+        }
+        // stop vert movement after ramp
+        if (other.gameObject.name == "RampTopTrigger") {
+            GM.vertVel = 0;
+        }
         //loads lvl complete scene when you hit exit collider
         if (other.gameObject.name == "Exit(Clone)") {
             SceneManager.LoadScene ("LevelComplete");
             GM.lvlCompStatus = "Success!";
         }
         //picks up coins
-        // if (other.gameObject.name == "Coin(Clone)" || other.gameObject.name == "Coin") {
-        //     Destroy(other.gameObject);
-        //     GM.coinTotal += 1;
-        // }
+        if (other.gameObject.name == "Coin(Clone)" || other.gameObject.name == "Coin") {
+            Destroy(other.gameObject);
+            GM.coinTotal += 1;
+        }
 
     }
 
-    // IEnumerator stopSlide() {
-    //     yield return new WaitForSeconds(.5f);
-    //     hvelocity = 0;
-    //     controlLocked = "n";
-    // }
+    IEnumerator stopSlide() {
+        yield return new WaitForSeconds(.5f);
+        hvelocity = 0;
+        controlLocked = "n";
+    }
 }
