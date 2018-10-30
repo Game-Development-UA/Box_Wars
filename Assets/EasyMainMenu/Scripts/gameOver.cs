73d2d4349e6a3fe742497ef7f10d130354e4fc2e
Vishal Shelat <vishalshelat@gmail.com>
2018-10-30 16:36:51 -0700

    Added win trigger

---
+++ Assets/EasyMainMenu/Scripts/gameOver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameOver : MonoBehaviour {
+
+void OnCollisionEnter( Collision c){
+    if (c.transform.name == "Player")
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    
+}
+}
