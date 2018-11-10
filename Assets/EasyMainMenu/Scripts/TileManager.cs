92112fc8fdc7304b2ebe69b037578d0ce98d8888
Vishal Shelat <vishalshelat@gmail.com>
2018-11-10 13:26:20 -0700

    Brought back all the changes I did
    sorry for reverting them

---
+++ Assets/EasyMainMenu/Scripts/TileManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+    public GameObject[] tilePrefabs;
+    
+    private Transform playerTransform;
+    
+    private float spawnZ = 0.0f;
+    
+    private float tileLength = 69.7f;
+	// Use this for initialization
+	void Start () {
+        playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+		
+        SpawnTile(); 
+        
+        SpawnTile();
+        
+        SpawnTile();
+        
+        SpawnTile();     
+        
+        
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+    
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(tilePrefabs [0]) as GameObject;
+        go.transform.SetParent (transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+    }
+}
---
+++ Assets/onhit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onhit : MonoBehaviour {
+
+	public void OnCollisionEnter(Collision onhit){
+        if (onhit.collider.gameObject.name == "Player"){          
+            Destroy(onhit.collider.gameObject);
+            
+
+        }
+ 
+    }
+    
+	void Update () {
+		
+	}
+}
