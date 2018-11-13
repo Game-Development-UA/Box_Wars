a65fb02cdec6edb63fc0cf3f8cfe661bea766b4d
Vishal Shelat <vishalshelat@gmail.com>
2018-11-13 15:34:16 -0700

    Added slow mechanic code

---
+++ Assets/EasyMainMenu/Scripts/slowonhit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slowonhit : MonoBehaviour {
+
+	public void OnCollisionEnter(Collision slowonhit){
+        if (onhit.collider.gameObject.name == "Player"){          
+            float onhit.collider.getComponent.name -= 3;
+            
+
+        }
+ 
+    }
+    
+	void Update () {
+		
+	}
+}
