cc9ecadf771d46b73c50503d470db40e8559761f
Vishal Shelat <vishalshelat@gmail.com>
2018-12-04 16:33:49 -0700

    added coroutine

---
+++ Assets/EasyMainMenu/Scripts/coroutine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coroutine : MonoBehaviour {
+public GameObject door_2_left; 
+
+    UnityEngine.Coroutine corout;
+    
+    void OnCollisonEnter(Collision col) {
+        if (col.gameObject.name == "Player") {
+            corout = StartCoroutine(HideObject());
+            print("test1");
+        }
+    }
+    
+    void OnCollisonExit(Collision col) {
+        StopCoroutine (corout);
+        print("test2");
+    }
+    
+    IEnumerator HideObject () {
+        yield return new WaitForSeconds(3);
+        door_2_left.SetActive(false);
+        print("test3");
+    }
+}
+           
